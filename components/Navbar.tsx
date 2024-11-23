f0e7c6b82b46be872f8884c7ccbec6490bb4afc4
YaserAsaad <yaserasaad500@gmail.com>
2024-11-23 14:51:23 +0300

    more z index for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,6 @@
 
 // import CustomButton from "./CustomButton";
 
-
 // const NavBar = () => (
 //   <header className="w-full bg-white fixed z-10">
 //     <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
@@ -17,13 +16,13 @@
 //         />
 //       </Link>
 
-      // <ul className="flex items-center gap-x-2">
-      //   {links.map((link) => (
-      //     <li className="py-1 px-2" key={link.link + link.label}>
-      //       <Link href={link.link}>{link.label}</Link>
-      //     </li>
-      //   ))}
-      // </ul>
+// <ul className="flex items-center gap-x-2">
+//   {links.map((link) => (
+//     <li className="py-1 px-2" key={link.link + link.label}>
+//       <Link href={link.link}>{link.label}</Link>
+//     </li>
+//   ))}
+// </ul>
 
 //       {/* <CustomButton
 //         title="Sign in"
@@ -61,25 +60,25 @@ const links = [
 ];
 
 const NavBar = () => (
-  <header className='w-full bg-black fixed z-10'>
-    <nav className=' max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
-      <Link href='/' className='flex justify-center items-center'>
-         <Image
-           src="/logo.png"
-            alt="logo"
-           width={100}
-           height={18}
-           className="object-contain"
-         />
+  <header className="w-full bg-black fixed z-40">
+    <nav className=" max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent">
+      <Link href="/" className="flex justify-center items-center">
+        <Image
+          src="/logo.png"
+          alt="logo"
+          width={100}
+          height={18}
+          className="object-contain"
+        />
       </Link>
 
-       <ul className="flex items-center gap-x-2">
-         {links.map((link) => (
-           <li className="py-1 px-2 text-white" key={link.link + link.label}>
-             <Link href={link.link}>{link.label}</Link>
-           </li>
-         ))}
-       </ul>
+      <ul className="flex items-center gap-x-2">
+        {links.map((link) => (
+          <li className="py-1 px-2 text-white" key={link.link + link.label}>
+            <Link href={link.link}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
     </nav>
   </header>
 );
